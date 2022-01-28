f03ccd9b5377b7ff868c089aeb525d8a98bbc21e
rafaelarissa <rafaela.larissalj@gmail.com>
2022-01-28 01:26:57 -0300

    feat: body of request POST /participants receives name

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from "dotenv";
+import { MongoClient } from 'mongodb';
 
 const server = express();
+server.use(express.json());
+server.use(cors());
+dotenv.config();
 
-server.get('/', (req, res) => {
-  res.send('Hello World');
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+  db = mongoClient.db("projeto12-API-bate-papo-UOL");
+});
+
+
+server.post('/participants', (req, res) => {
+  db.collection("participants").insertOne(req.body).then(() => {
+    res.sendStatus(201);
+  });
+});
+
+server.get('/participants', (req, res) => {
+  db.collection("participants").find().toArray().then(participant => {
+    res.send(participant);
+  });
 });
 
 server.listen(5000);
